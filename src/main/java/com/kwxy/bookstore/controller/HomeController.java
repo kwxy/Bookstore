b1f518c9dd04ca6bd6765c0f8097c31e80f6b846
kwxy <monikakatarzynazajac@gmail.com>
2017-06-28 20:11:54 +0200

    Added home controller

---
+++ src/main/java/com/kwxy/bookstore/controller/HomeController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kwxy.bookstore.controller;
+
+import com.kwxy.bookstore.database.Book;
+import com.kwxy.bookstore.database.BookCategory;
+import com.kwxy.bookstore.services.BookService;
+import com.kwxy.bookstore.services.ClientService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Monika
+ */
+@Controller
+public class HomeController {
+
+    @Autowired
+    ClientService clientService;
+
+    @Autowired
+    BookService bookService;
+
+    public List<BookCategory> getAllCategories() {
+        return bookService.getAllBookCategory();
+    }
+
+    public List<Book> getBooksByCategoryId(Integer id) {
+        return bookService.getBooksByCategoryId(id);
+    }
+
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    public Book getBooksById(Integer id) {
+        return bookService.getBooksById(id);
+    }
+
+    @RequestMapping(value = "/{currentCategoryId}", method = RequestMethod.GET)
+    public String mainPage(Model model, @PathVariable("currentCategoryId") int currentCategoryId) {
+        model.addAttribute("categories", getAllCategories());
+        model.addAttribute("books", getBooksByCategoryId(currentCategoryId));
+        return "index";
+    }
+}
